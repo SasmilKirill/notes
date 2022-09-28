25aa1d7016711c3b9f6776942b818f928a3ea18d
KirillSasmil <kir.sasmil2002@gmail.com>
2022-09-28 19:10:36 +0300

    add files

--- authors/serialiazers.py
+++ authors/serialiazers.py
@@ -1,11 +1,11 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Author
+#from rest_framework.serializers import ModelSerializer
+#from .models import Author
 
 
 
-class AuthorModelSerializer(ModelSerializer)
-    class Meta:
-        model = Author
-        fielsd = 'user_name, first_name, last_name, email '
+#class AuthorModelSerializer(ModelSerializer)
+   # class Meta:
+    #    model = Author
+    #    fielsd = 'user_name, first_name, last_name, email '
         # fields = ('first_name',last_name')
         # excluse = ('last_name',)
--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+#from django.contrib import admin
+#from django.urls import path, include
 
 
-router = DefaultRouter()
-router.register('authors', AuthorsModelViewSet)
+#router = DefaultRouter()
+#router.register('authors', AuthorsModelViewSet)
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth', include('rest_framework.urls')),
-    path('api/', include(router.urls))
+#urlpatterns = [
+   # path('admin/', admin.site.urls),
+   # path('api/auth', include('rest_framework.urls')),
+   # path('api/', include(router.urls))
 ]
