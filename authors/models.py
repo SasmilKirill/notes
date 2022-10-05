2b738b6f873476317cc39f4e3096cdff9a30618a
KirillSasmil <kir.sasmil2002@gmail.com>
2022-10-05 15:28:51 +0300

    add files

--- authors/models.py
+++ authors/models.py
@@ -12,3 +12,16 @@ class Author(models.Model):
 
 Class User(AbstractUser):
      image = models.ImageField(upload_to='users_image',blank=True)
+
+
+def __str__(self):
+    return f'{ -last_name} { .first_name} { .birthday_year}'
+
+class Biographies(models.Model):
+    text = models.TextField()
+    author = models.OneToOneField(Author,on_delete=models.CASCADE)
+
+class Book (models.Model):
+name = models.CharFieldt(max_lrnght=64)
+author = models.ManyToManyField(Author)
+
--- authors/views.py
+++ authors/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Author
+from .models import Author,Book,BiographiesHyperlinkedModeSerializer
 from .serialiazers import AuthorModelSerializer
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+class BiographiesModelViewSet(ModelViewSet):
+    queryset = Biographies.objects.all()
+    serializer_class = BiographiesHyperlinkedModeSerializer
 
     #def get_queryset(selfself):
      #   return Author.objects.get(id=1)
